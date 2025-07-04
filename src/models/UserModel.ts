44495ce1be79512dc1bc84c02f3814118dbb736b
Rafik <rafik11111@yahoo.com>
2025-07-04 12:52:30 +0300

    Update User Model to complete the schema.

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,15 +1,32 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+export enum UserRole {
+  VENDOR = 'vendor',  
+  USER = 'user',
+}
+
 export interface IUser extends Document {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  phone: string;
   password: string;
+  profilePicture: string;
+  role: UserRole;
 }
 
 const UserSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, 
+  profilePicture: { type: String, required: false },
+  role: { type: String, enum: Object.values(UserRole) },
+});
+
+UserSchema.virtual('fullName').get(function() {
+  return `${this.firstName} ${this.lastName}`;
 });
 
 export default mongoose.model<IUser>('User', UserSchema); 
