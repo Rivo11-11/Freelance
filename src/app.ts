8d4f8c5d8fe24deabbfc800a6039b5527b624d60
Rafik <rafik11111@yahoo.com>
2025-07-10 20:59:42 +0300

    Try documentation on production .

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,47 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Create a new route for swagger docs
+app.get('/api-docs', (_, res) => {
+  const swaggerUIHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>API Documentation</title>
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui.css" />
+  <style>
+    html { box-sizing: border-box; overflow: -moz-scrollbars-vertical; overflow-y: scroll; }
+    *, *:before, *:after { box-sizing: inherit; }
+    body { margin:0; background: #fafafa; }
+  </style>
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function() {
+      SwaggerUIBundle({
+        url: window.location.origin + '/swagger.json',
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+      });
+    };
+  </script>
+</body>
+</html>`;
 
+  res.setHeader('Content-Type', 'text/html');
+  res.send(swaggerUIHTML);
+});
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
